c75cb3aee82c53bb34a24e1c276c47d8f7762237
Conal Elliott <conal@conal.net>
2010-10-11 15:54:31 -0700

    added the other files

---
+++ src/Network/Gitit/Plugin/FixSymbols.hs
@@ -0,0 +1,62 @@
+-- {-# LANGUAGE #-}
+{-# OPTIONS_GHC -Wall #-}
+----------------------------------------------------------------------
+-- |
+-- Module      :  Network.Gitit.Plugin.FixSymbols
+-- Copyright   :  (c) Conal Elliott 2010
+-- License     :  BSD3
+-- 
+-- Maintainer  :  conal@conal.net
+-- Stability   :  experimental
+-- 
+-- Turn some Haskell symbols into pretty math symbols
+----------------------------------------------------------------------
+
+module Network.Gitit.Plugin.FixSymbols (plugin) where
+
+import Network.Gitit.Interface
+
+import Data.List (isPrefixOf)
+
+plugin :: Plugin
+plugin = PageTransform $ return . processWith fixInline . processWith fixBlock
+
+-- mkPageTransform :: Data a => (a -> a) -> Plugin
+-- mkPageTransform fn = PageTransform $ return . processWith fn
+
+
+fixInline :: Inline -> Inline
+fixInline (Code s) = Code (codeSubst s)
+fixInline x        = x
+
+fixBlock :: Block -> Block
+fixBlock (CodeBlock attr@(_,classes,_) s)
+  | "haskell" `elem` classes = CodeBlock attr (codeSubst s)
+fixBlock x = x
+
+-- TODO: transform lexemes instead of strings to avoid things like "-->"
+-- becoming "-→".  Use the Text.Read.Lex module in Base.  Hm.  How to
+-- reconstruct white space & comments?
+
+codeSubst :: String -> String
+codeSubst = substs [ ("forall","∀"),("->","→"),(":*","×")
+                   , ("\\","λ"),("`lub`","⊔"),("`glb`","⊓")
+                   , ("undefined","⊥"), ("bottom","⊥")
+                   , ("<-","←"), ("::","∷"), ("..","‥"), ("...","⋯")
+                   ]
+
+-- TODO: Faster substitution.  Turn the from/to pairs into a single, fast
+-- automaton.  I could also switch to a single-pass algorithm, instead of
+-- one pass per from/to pair.
+
+subst :: String -> String -> String -> String
+subst from to = sub
+ where
+   sub :: String -> String
+   sub "" = ""
+   sub str | from `isPrefixOf` str = to ++ sub (drop n str)
+   sub (c:cs) = c : sub cs
+   n = length from
+
+substs :: [(String, String)] -> String -> String
+substs = foldr (.) id . map (uncurry subst)
